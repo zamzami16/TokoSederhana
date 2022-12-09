b640b2a8eac79b72b572a0f6cba2f8dae6010f28
zamzami16 <mohammad.yusuf.zamzami-2016@fst.unair.ac.id>
2022-12-09 21:38:25 +0700

    Implementasi pencarian data

--- TokoSederhana/TambahDataBarang.Designer.cs
+++ TokoSederhana/TambahDataBarang.Designer.cs
@@ -32,9 +32,9 @@ namespace TokoSederhana
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.textBoxHargaJual = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.textBoxKeuntungan = new System.Windows.Forms.TextBox();
             this.textBoxHargaBeli = new System.Windows.Forms.TextBox();
             this.buttonTambahDataBarang = new System.Windows.Forms.Button();
@@ -46,13 +46,21 @@ namespace TokoSederhana
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.contextMenuStrip1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -61,39 +69,24 @@ namespace TokoSederhana
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 53);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(884, 461);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 411F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(878, 465);
             this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(179, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(612, 44);
-            this.panel1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(133, 6);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(286, 29);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "TAMBAH DATA BARANG";
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.buttonSearch);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.textBoxSearch);
+            this.panel2.Controls.Add(this.textBoxHargaJual);
+            this.panel2.Controls.Add(this.label7);
             this.panel2.Controls.Add(this.textBoxKeuntungan);
             this.panel2.Controls.Add(this.textBoxHargaBeli);
             this.panel2.Controls.Add(this.buttonTambahDataBarang);
@@ -105,10 +98,28 @@ namespace TokoSederhana
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(3, 53);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(170, 405);
+            this.panel2.Size = new System.Drawing.Size(169, 459);
             this.panel2.TabIndex = 1;
+            // 
+            // textBoxHargaJual
+            // 
+            this.textBoxHargaJual.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxHargaJual.Location = new System.Drawing.Point(13, 242);
+            this.textBoxHargaJual.Name = "textBoxHargaJual";
+            this.textBoxHargaJual.Size = new System.Drawing.Size(202, 21);
+            this.textBoxHargaJual.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(10, 224);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(144, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "TETAPKAN HARGA JUAL";
             // 
             // textBoxKeuntungan
             // 
@@ -129,7 +140,7 @@ namespace TokoSederhana
             // 
             // buttonTambahDataBarang
             // 
-            this.buttonTambahDataBarang.Location = new System.Drawing.Point(13, 234);
+            this.buttonTambahDataBarang.Location = new System.Drawing.Point(13, 282);
             this.buttonTambahDataBarang.Name = "buttonTambahDataBarang";
             this.buttonTambahDataBarang.Size = new System.Drawing.Size(137, 32);
             this.buttonTambahDataBarang.TabIndex = 2;
@@ -208,22 +219,39 @@ namespace TokoSederhana
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.AliceBlue;
             dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.PaleTurquoise;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Blue;
             this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(179, 53);
+            this.dataGridView1.Location = new System.Drawing.Point(178, 3);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(612, 405);
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(608, 459);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentDoubleClick);
             this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView1_CellPainting);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(872, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TAMBAH DATA BARANG";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // contextMenuStrip1
             // 
@@ -239,22 +267,97 @@ namespace TokoSederhana
             this.toolStripMenuItem1.Text = "Delete Row";
             this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
             // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(884, 541);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 524);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(200, 14);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 1;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel4.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(878, 44);
+            this.tableLayoutPanel4.TabIndex = 2;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.Location = new System.Drawing.Point(12, 375);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(202, 21);
+            this.textBoxSearch.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(10, 357);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(146, 15);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "MASUKKAN KATA KUNCI";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(13, 402);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(137, 36);
+            this.buttonSearch.TabIndex = 14;
+            this.buttonSearch.Text = "SEARCH";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
             // TambahDataBarang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 461);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(884, 541);
+            this.Controls.Add(this.tableLayoutPanel2);
             this.Name = "TambahDataBarang";
             this.Text = "TambahDataBarang";
             this.Load += new System.EventHandler(this.TambahDataBarang_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.contextMenuStrip1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -262,7 +365,6 @@ namespace TokoSederhana
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBoxKeuntungan;
@@ -278,5 +380,13 @@ namespace TokoSederhana
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.TextBox textBoxHargaJual;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxSearch;
     }
 }
--- TokoSederhana/TambahDataBarang.cs
+++ TokoSederhana/TambahDataBarang.cs
@@ -15,6 +15,8 @@ namespace TokoSederhana
         {
             InitializeComponent();
             initColumnTable();
+            textBoxNamaBarang.CharacterCasing = CharacterCasing.Upper;
+            textBoxKodeBarang.CharacterCasing = CharacterCasing.Upper;
         }
 
         // Add Row selected
@@ -30,17 +32,34 @@ namespace TokoSederhana
             this.sourceDataGrid.Columns.Add("NAMA BARANG", typeof(string));
             this.sourceDataGrid.Columns.Add("HARGA BELI", typeof(double));
             this.sourceDataGrid.Columns.Add("HARGA JUAL", typeof(double));
+            this.sourceDataGrid.Columns.Add("TANGGAL", typeof(DateTime));
 
             dataGridView1.DataSource = this.sourceDataGrid;
+            dataGridView1.Columns[0].FillWeight = 10;
+            dataGridView1.Columns[1].FillWeight = 15;
+            dataGridView1.Columns[2].FillWeight = 35;
+            dataGridView1.Columns[3].FillWeight = 13;
+            dataGridView1.Columns[4].FillWeight = 13;
+            dataGridView1.Columns[5].FillWeight = 14;
+            //dataGridView1.RowHeadersVisible = false;
+
+            ForDebuging();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void ForDebuging()
         {
-
+            TambahDataKeGrid(1, "C540", "MASAKO", 2500, 3000);
+            TambahDataKeGrid(2, "C541", "MASAKO SAPI BESAR", 2500, 3000);
+            TambahDataKeGrid(3, "C542", "MASAKO SAPI KECIL", 2500, 3000);
+            TambahDataKeGrid(4, "C543", "MASAKO AYAM BESAR", 2500, 3000);
+            TambahDataKeGrid(5, "C544", "MASAKO AYAM KAMPUNG", 2500, 3000);
         }
+
+        private void label2_Click(object sender, EventArgs e) { }
 
         private void buttonTambahDataBarang_Click(object sender, EventArgs e)
         {
+            dataGridView1.DataSource = this.sourceDataGrid;
             int idx;           
             string kode = textBoxKodeBarang.Text;
             string namaBarang = textBoxNamaBarang.Text;
@@ -106,6 +125,7 @@ namespace TokoSederhana
             drow["NAMA BARANG"] = nama_barang_;
             drow["HARGA BELI"] = hargaBarang_;
             drow["HARGA JUAL"] = hargaJual_;
+            drow["TANGGAL"] = DateTime.UtcNow.ToShortDateString();
             this.sourceDataGrid.Rows.Add(drow);
 
             HapusIsianTambahData();
@@ -117,6 +137,7 @@ namespace TokoSederhana
             this.sourceDataGrid.Rows[idx_]["NAMA BARANG"] = nama_barang_;
             this.sourceDataGrid.Rows[idx_]["HARGA BELI"] = hargaBarang_;
             this.sourceDataGrid.Rows[idx_]["HARGA JUAL"] = hargaJual_;
+            this.sourceDataGrid.Rows[idx_]["TANGGAL"] = DateTime.UtcNow.ToShortDateString();
 
             HapusIsianTambahData();
         }
@@ -196,6 +217,75 @@ namespace TokoSederhana
             {
                 //this.dataGridView1.Rows.RemoveAt(this.rowSelectedForEdit);
                 this.sourceDataGrid.Rows.RemoveAt(this.rowSelectedForEdit);
+
+            }
+        }
+
+        private DataRow[] SearchValue(string keyWord)
+        {
+            DataRow[] results = this.sourceDataGrid.Select($"NAMA BARANG Like '%{keyWord}%' or KODE Like '%{keyWord}%' or HARGA BELI Like '%{keyWord}%' or HARGA JUAL Like '%{keyWord}%'");
+            return results;
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            DataTable tempDataView = SelectSearchData(textBoxSearch.Text, this.sourceDataGrid);
+            dataGridView1.DataSource = tempDataView;
+        }
+
+        private DataTable SelectSearchData(string keyWord, DataTable dt)
+        {
+            DataView dataView = new DataView(dt);
+            //dataView.RowFilter = $"'NAMA BARANG' Like '%{keyWord}%' or KODE Like '%{keyWord}%' or 'HARGA BELI' Like '%{keyWord}%' or 'HARGA JUAL' Like '%{keyWord}%'";
+            dataView.RowFilter = "[NAMA BARANG] LIKE '%" + keyWord + "%'";
+            return dataView.ToTable();
+        }
+
+        private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            // High light and searching apply over selective fields of grid.  
+            if (e.RowIndex > -1 && e.ColumnIndex > -1 && this.sourceDataGrid.Columns[e.ColumnIndex].ColumnName != "NO")
+            {
+                // Check data for search  
+                if (!String.IsNullOrWhiteSpace(textBoxSearch.Text.Trim()))
+                {
+                    String gridCellValue = e.FormattedValue.ToString();
+                    // check the index of search text into grid cell.  
+                    int startIndexInCellValue = gridCellValue.ToLower().IndexOf(textBoxSearch.Text.Trim().ToLower());
+                    // IF search text is exists inside grid cell then startIndexInCellValue value will be greater then 0 or equal to 0  
+                    if (startIndexInCellValue >= 0)
+                    {
+                        e.Handled = true;
+                        e.PaintBackground(e.CellBounds, true);
+                        //the highlite rectangle  
+                        Rectangle hl_rect = new Rectangle();
+                        hl_rect.Y = e.CellBounds.Y + 2;
+                        hl_rect.Height = e.CellBounds.Height - 5;
+                        //find the size of the text before the search word in grid cell data.  
+                        String sBeforeSearchword = gridCellValue.Substring(0, startIndexInCellValue);
+                        //size of the search word in the grid cell data  
+                        String sSearchWord = gridCellValue.Substring(startIndexInCellValue, textBoxSearch.Text.Trim().Length);
+                        Size s1 = TextRenderer.MeasureText(e.Graphics, sBeforeSearchword, e.CellStyle.Font, e.CellBounds.Size);
+                        Size s2 = TextRenderer.MeasureText(e.Graphics, sSearchWord, e.CellStyle.Font, e.CellBounds.Size);
+                        if (s1.Width > 5)
+                        {
+                            hl_rect.X = e.CellBounds.X + s1.Width - 5;
+                            hl_rect.Width = s2.Width - 6;
+                        }
+                        else
+                        {
+                            hl_rect.X = e.CellBounds.X + 2;
+                            hl_rect.Width = s2.Width - 6;
+                        }
+                        //color for showing highlighted text in grid cell  
+                        SolidBrush hl_brush;
+                        hl_brush = new SolidBrush(Color.Yellow);
+                        //paint the background behind the search word  
+                        e.Graphics.FillRectangle(hl_brush, hl_rect);
+                        hl_brush.Dispose();
+                        e.PaintContent(e.CellBounds);
+                    }
+                }
             }
         }
     }
